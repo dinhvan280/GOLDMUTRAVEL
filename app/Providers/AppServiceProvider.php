53deff8139f5413efaf48eb2d4960ac558cad8d7
Kim Long <ngokimlong1502@gmail.com>
2022-06-15 00:59:50 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,9 +28,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-//        if(env('APP_ENV') != 'local'){
-//            $this->app['request']->server->set('HTTPS','on');
-//        }
+        if(env('APP_ENV') != 'local'){
+            $this->app['request']->server->set('HTTPS','on');
+        }
         View::share('key', 'value');
         Schema::defaultStringLength(191);
 
